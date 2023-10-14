beddd7db5b8c7c41dd8911a00a5ab02f50350980
Ky-AnT <87473241+Ky-AnT@users.noreply.github.com>
2023-10-14 18:20:44 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+userInput = input("Enter a number to see if it is a palindrome: ")
+if len(userInput)%2==0:
+    half = userInput[0:int((len(userInput)/2))]
+    if half+half[::-1] != userInput:
+        print(f"{userInput} is not a palindrome!")
+    else:
+        print(f"{userInput} is a palindrome!")
+else:
+    half = userInput[0:int((len(userInput)/2))]
+    if half+userInput[int((len(userInput)/2))]+half[::-1] != userInput:
+        print(f"{userInput} is not a palindrome!")
+    else:
+        print(f"{userInput} is a palindrome!")
