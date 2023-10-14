1b39db2bec4578be0313f5889fe6ce6504096eac
Ky-AnT <87473241+Ky-AnT@users.noreply.github.com>
2023-10-14 18:26:16 +0800

    Update main.py
    Changed 'number' to 'word' (typo)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-userInput = input("Enter a number to see if it is a palindrome: ")
+userInput = input("Enter a word to see if it is a palindrome: ")
 if len(userInput)%2==0:
     half = userInput[0:int((len(userInput)/2))]
     if half+half[::-1] != userInput:
